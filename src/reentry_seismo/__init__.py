36fccb56947a6f2cc22c711122907eaac99d4162
jsteinberg34 <josephsteinberg933@gmail.com>
2026-04-21 16:10:30 -0400

    Added main run_pipeline() function

--- src/reentry_seismo/__init__.py
+++ src/reentry_seismo/__init__.py
@@ -14,3 +14,4 @@ from .stations import load_and_filter_stations
 from .plotting import plot_track_and_boxes, plot_stations, plot_station_comparison
 from .tiling import track_to_box_windows, box_windows_to_download_requests
 from .processing import process_boxes, process_box, process_stream
+from .pipeline import run_pipeline
